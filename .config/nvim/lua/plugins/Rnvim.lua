33ec309215b9d5ef2dd1a1b7a6813f5f9e0720ac
Iago Mosqueira <iagomosqueira@users.noreply.github.com>
2025-01-08 09:10:24 +0100

    Wed  8 Jan 09:10:24 CET 2025

--- .config/nvim/lua/plugins/Rnvim.lua
+++ .config/nvim/lua/plugins/Rnvim.lua
@@ -1,6 +1,7 @@
 require("r").setup{
   R_args = {"--no-save"},
   objbr_openlist = false,
+  pdfviewer = "xdg-open",
   objbr_w = 16,
   setwd = "file",
   external_term = "alacritty --config-file ~/.config/alacritty/R.yml -e"
